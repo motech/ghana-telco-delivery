23247aed6a9251060dbcf20c9cf84487af9cb625
Arvind Kumar C <arvindc@thoughtworks.com>
2012-05-21 04:17:02 +0530

    Arvind | Modified deployment scripts

--- sql/tables_postgres.sql
+++ sql/tables_postgres.sql
@@ -3,19 +3,6 @@
 -- In your Quartz properties file, you'll need to set 
 -- org.quartz.jobStore.driverDelegateClass = org.quartz.impl.jdbcjobstore.PostgreSQLDelegate
 
-drop table if exists qrtz_job_listeners;
-drop table if exists qrtz_trigger_listeners;
-drop table if exists qrtz_fired_triggers;
-DROP TABLE if exists QRTZ_PAUSED_TRIGGER_GRPS;
-DROP TABLE if exists QRTZ_SCHEDULER_STATE;
-DROP TABLE if exists QRTZ_LOCKS;
-drop table if exists qrtz_simple_triggers;
-drop table if exists qrtz_cron_triggers;
-DROP TABLE if exists QRTZ_BLOB_TRIGGERS;
-drop table if exists qrtz_triggers;
-drop table if exists qrtz_job_details;
-drop table if exists qrtz_calendars;
-
 CREATE TABLE qrtz_job_details
   (
     JOB_NAME  VARCHAR(250) NOT NULL,
